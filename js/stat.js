29e2dfa731c431b8bc9d490ecf2906d313efd623
Matveeva Irina <matveeva-83@mail.ru>
2018-06-18 19:08:35 +0300

    Гистограмма с исправлением

--- js/stat.js
+++ js/stat.js
@@ -14,7 +14,6 @@ var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
-
 var getMaxElement = function (arr) {
   var maxElement = arr[0];
 
@@ -23,25 +22,17 @@ var getMaxElement = function (arr) {
       maxElement = arr[i];
     }
   }
-
   return maxElement;
 };
-
 window.renderStatistics = function (ctx, players, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
-
-
-
   ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-
   ctx.font = '16px PT Mono';
   ctx.textBaseline = 'hanging';
   ctx.fillText('Ура вы победили!', 105, 15);
   ctx.fillText('Список результатов:', 105, 35);
-
   var maxTime = getMaxElement(times);
-
   for (var i = 0; i < players.length; i++) {
     var randomOpacity = (Math.random() * 0.9 + 0.1);
     if (players[i] === 'Вы') {
@@ -49,8 +40,9 @@ window.renderStatistics = function (ctx, players, times) {
     } else {
       ctx.fillStyle = 'rgba(0, 0, 255, ' + randomOpacity + ')';
     }
-    ctx.fillText(Math.round(times[i]), CLOUD_X + (GAP  + TEXT_WIDTH) * i, CLOUD_Y + GAP);
+    ctx.fillText(Math.round(times[i]), CLOUD_X + (GAP + TEXT_WIDTH) * i, CLOUD_Y + GAP);
     ctx.fillText(players[i], CLOUD_X + (GAP + TEXT_WIDTH) * i, CLOUD_Y + GAP + BAR_HEIGHT + GAP);
     ctx.fillRect(CLOUD_X + (GAP + TEXT_WIDTH) * i, CLOUD_Y + GAP + FONT_GAP + (170 - ((BAR_HEIGHT * times[i]) / maxTime)), barWidth, (BAR_HEIGHT * times[i]) / maxTime);
 
-  };}
+  }
+};
